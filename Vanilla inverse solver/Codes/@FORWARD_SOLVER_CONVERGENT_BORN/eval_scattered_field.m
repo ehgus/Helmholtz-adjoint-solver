28c500b7d530f404c4c7796530dad927f74c3e48
Dohyeon Lee <dleh428@kaist.ac.kr>
2022-07-22 02:44:06 +0900

    Pre-evaluation of green function

--- Vanilla inverse solver/Codes/@FORWARD_SOLVER_CONVERGENT_BORN/eval_scattered_field.m
+++ Vanilla inverse solver/Codes/@FORWARD_SOLVER_CONVERGENT_BORN/eval_scattered_field.m
@@ -18,20 +18,31 @@ function Field=eval_scattered_field(h,source)
         source00 = source;
         source(:) = 0;
         for j1 = 1:3
-            source = source + (h.V(h.ROI(1):h.ROI(2),h.ROI(3):h.ROI(4),(h.ROI(5)):(h.ROI(6)),:,j1)) .* source00(:,:,:,j1);
+            source = source + (h.V(h.ROI(1):h.ROI(2),h.ROI(3):h.ROI(4),(h.ROI(5)):(h.ROI(6)),:,j1)+1i*h.eps_imag) .* source00(:,:,:,j1);
         end
-        source = source+1i*h.eps_imag*source00;
         clear source00
     end
+    
+    Greenp = h.Greenp;
+    if h.pole_num==3
+        [xsize, ysize, zsize] = size(Greenp);
+        Greenp = Greenp.*(h.eye_3-h.green_absorbtion_correction*(h.rads).*reshape(h.rads,xsize,ysize,zsize,1,3));
+    end
+    if h.parameters.acyclic
+        flip_Greenp = fft_flip(h.Greenp,[1 1 1],false);
+        if h.pole_num==3
+            flip_rads = fft_flip(h.rads,[1 1 1],false);
+            [xsize, ysize, zsize] = size(flip_Greenp);
+            flip_Greenp = flip_Greenp.*(h.eye_3-h.green_absorbtion_correction*(flip_rads).*reshape(flip_rads,xsize,ysize,zsize,1,3));
+        end
+    end
+    conj_phase_ramp=conj(h.phase_ramp);
     
     for jj = 1:h.Bornmax
         %flip the relevant quantities
         if h.parameters.acyclic
-            h.Greenp=fft_flip(h.Greenp,[1 1 1],false);
-            if h.pole_num==3
-                h.rads=fft_flip(h.rads,[1 1 1],false);
-            end
-            h.phase_ramp=conj(h.phase_ramp);
+            [Greenp, flip_Greenp] = deal(flip_Greenp, Greenp);
+            [h.phase_ramp, conj_phase_ramp] = deal(conj_phase_ramp, h.phase_ramp);
         end
         
         %init other quantities
@@ -43,25 +54,26 @@ function Field=eval_scattered_field(h,source)
             psi(h.ROI(1):h.ROI(2),h.ROI(3):h.ROI(4),h.ROI(5):h.ROI(6),:,:) = (1i./h.eps_imag).*source/2;
         else % gamma * E
             if size(h.V,4) == 1
-                psi = (1i./h.eps_imag) .* Field_n .* h.V;
+                psi = h.V .* Field_n;
             else
                 for j1 = 1:3
-                    psi = psi + (1i./h.eps_imag) .* Field_n(:,:,:,j1) .* h.V(:,:,:,:,j1);
+                    psi = psi + h.V(:,:,:,:,j1) .* Field_n(:,:,:,j1);
                 end
             end
+            psi = (1i./h.eps_imag).*psi;
         end
         
         % G x
         for j2 = 1:h.pole_num
             coeff_field=fftn(psi(:,:,:,j2).*h.phase_ramp);
             if h.pole_num==3 %dyadic absorptive green function convolution
-                PSI = PSI + ((h.Greenp.*(h.eye_3(:,:,:,:,j2)-h.green_absorbtion_correction*(h.rads).*(h.rads(:,:,:,j2)))).* coeff_field);
-            else %dscalar absorptive green function convolution
-                PSI = (h.Greenp.*coeff_field);
+                PSI = PSI + Greenp(:,:,:,:,j2).* coeff_field;
+            else %scalar absorptive green function convolution
+                PSI = Greenp.*coeff_field;
             end
         end
         for j1 = 1:h.pole_num
-            PSI(:,:,:,j1)=ifftn(PSI(:,:,:,j1)).*conj(h.phase_ramp);
+            PSI(:,:,:,j1)=ifftn(PSI(:,:,:,j1)).*conj_phase_ramp;
         end
         if ~any(jj == [1,2])
             Field_n = Field_n - psi;
