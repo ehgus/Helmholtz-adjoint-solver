0be90c03242cdd7494d95fc795a30e6bbadd8adc
Dohyeon Lee <dleh428@kaist.ac.kr>
2022-08-10 11:11:42 +0900

    Revert "Remove unnecceary code"
    This reverts commit 6978ec7a7da5e720357fae8fe9288baa223df7d7.

--- Vanilla inverse solver/Codes/BACKWARD_SOLVER_MULTI_FAST.m
+++ Vanilla inverse solver/Codes/BACKWARD_SOLVER_MULTI_FAST.m
@@ -1,9 +1,8 @@
-classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
+classdef BACKWARD_SOLVER_MULTI_FAST < BACKWARD_SOLVER
     properties (SetAccess = protected, Hidden = true)
         forward_solver;
         overlap_count;
         filter;
-        RI_inter;
     end
     methods(Static)
         function params=get_default_parameters(init_params)
@@ -13,21 +12,24 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
             params.forward_solver_parameters= FORWARD_SOLVER.get_default_parameters();
             params.init_solver=BACKWARD_SOLVER(BACKWARD_SOLVER.get_default_parameters());
             params.step=0.01;%0.01;0.01;%0.01;
-            params.tv_param=0.001;%0.1;
+            params.tv_param=0.005;%0.001;%0.1;
             params.use_non_negativity=false;
-            params.nmin = -inf;%1.336;
-            params.nmax = inf;%1.6;
+            params.nmin = 1.336;
+            params.nmax = 1.6;
             params.kappamax = 0; % imaginary RI
             params.inner_itt = 100; % imaginary RI
             params.itter_max = 100; % imaginary RI
             params.num_scan_per_iteration = 0; % 0 -> every scan is used
             params.verbose = true;
-            %params.filter_by_count=false;
+            params.filter_by_count=false;
             if nargin==1
                 params=update_struct(params,init_params);
             end
         end
-        function [gradient_RI,err]=get_gradiant_static(RI,input_field,output_field,forward_solver)
+        
+    end
+    methods
+        function [gradient_RI,err]=get_gradiant_static(h,RI,input_field,output_field,forward_solver)
             warning ('off','all');
             forward_solver.set_RI(RI);
             
@@ -35,13 +37,11 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
             %[source_3D,trans_source,~]
             diff_field=(trans_source-output_field);
             err=sum(abs(diff_field).^2,'all');
-            %{
             if h.parameters.filter_by_count
                 diff_field=fftshift(fft2(ifftshift(diff_field)));
-                diff_field=diff_field.*reshape(h.filter,size(h.filter,1),size(h.filter,2),1,[]).*size(diff_field,4)/5;
+                diff_field=diff_field.*reshape(h.filter,size(h.filter,1),size(h.filter,2),1,[]).*size(diff_field,4)/3;
                 diff_field=fftshift(ifft2(ifftshift(diff_field)));
             end
-            %}
             %err_list(end)
             %backpropagate
             warning ('off','all');
@@ -52,18 +52,13 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
             %recon_RI=(recon_RI.^2 / n_m^2 - 1);
             gradient_RI=-(1i.*((mean(conj(source_3D).*diff_3D,[4 5]))));
         end
-    end
-    methods
-        
-        function h=BACKWARD_SOLVER_MULTI(params,init_solver)
+        function h=BACKWARD_SOLVER_MULTI_FAST(params,init_solver)
             %do not set the init solver it is for porent class compatibility
             h@BACKWARD_SOLVER(params);
             if ~h.parameters.forward_solver_parameters.return_transmission || ...
                     ~h.parameters.forward_solver_parameters.return_reflection || ...
                     ~h.parameters.forward_solver_parameters.return_3D
-                h.parameters.forward_solver_parameters
                 error('need to set all return parameter to true in the forward solver');
-                
             end
             if ~exist('init_solver','var')
                 init_solver=true;
@@ -71,11 +66,9 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
             if init_solver
                 h.forward_solver=h.parameters.forward_solver(h.parameters.forward_solver_parameters);
             end
-            %{
             if h.parameters.filter_by_count
                 h.overlap_count=OVERLAP_COUNTER(OVERLAP_COUNTER.get_default_parameters(params));
             end
-            %}
         end
         function [gradient_RI,err]=get_gradiant(h,RI,input_field,output_field)
             
@@ -91,13 +84,15 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
         end
         function [RI]=solve(h,input_field,output_field)
             
+            error('also add a filter in the TV');
+            
             RI=single(h.parameters.init_solver.solve(input_field,output_field));
-            %{
+            
             if h.parameters.filter_by_count
                 [~,h.filter]=h.overlap_count.get_overlap(input_field);
                 h.filter(h.filter>0)=1./h.filter(h.filter>0);
             end
-            %}
+            
             err_list=[];
             
             dirichlet_boundary=false;
@@ -114,7 +109,7 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
             c_n=0;
             c_np=Inf;
             
-            if mod(size(RI,3),2)==0
+            if mod(size(RI),2)==0
                 error('need to be odd size');
             end
             if h.parameters.verbose
@@ -141,7 +136,6 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
                 tic;
                 
                 [gradient_RI,err_list(end+1)]=h.get_gradiant(RI,input_field,output_field);
-                %gradient_RI=real(gradient_RI);
                 %MFISTA
                 
                 t_n=t_np;
@@ -152,14 +146,11 @@ classdef BACKWARD_SOLVER_MULTI < BACKWARD_SOLVER
                 s_n=TV_FISTA_inner_v2(u_n-(1/alpha)*gradient_RI,h.parameters.tv_param/alpha,...
                     Vmin, Vmax, Vimag_max, h.parameters.use_non_negativity,dirichlet_boundary,h.parameters.inner_itt,use_gpu);
 %                 s_n=TV_FISTA_inner(u_n-(1/alpha)*gradient_RI,h.parameters.tv_param/alpha,h.parameters.use_non_negativity,dirichlet_boundary,h.parameters.inner_itt,use_gpu);
-                s_n=gather(s_n);
                 t_np=(1+sqrt(1+4*t_n^2))/2;
                 u_n=s_n+(t_n-1)/t_np*(s_n-x_n);
                 x_n=s_n;
                 RI=u_n;
                 RI=h.parameters.RI_bg *sqrt(RI+1);
-                
-                h.RI_inter=RI;
                 
                 toc;
                 
