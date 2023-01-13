5872d7f3e07a39f9041f9c6a5c89a29acbc72ff5
Dohyeon Lee <dleh428@kaist.ac.kr>
2023-01-13 11:26:09 +0900

    Explanatory metalens test

--- Vanilla inverse solver/test/Metalens_forward_validation.m
+++ Vanilla inverse solver/test/Metalens_forward_validation.m
@@ -1,6 +1,14 @@
 % Metalens simulator for optimized lens
+%
+% [execution guideline]
 % The following script needs mat file having a optimized RI profile
 % A example profile can be acquired after executing "ADJOINT_EXAMPLE.m"
+% After getting the metalens profile, rename "optimized_RI.h5" to "optimized_RI_no_pad.h5"
+%
+% [Result interpretation]
+% Note that the FDTD result will shows a weak light focus compared to the counterpart of CBS
+% It is expected because FDTD does not have enough grid to simulate accurately
+% To get more precise results, you need to change to following code slightly, but also the author will add the code in near future
 
 clc, clear;close all
 dirname = fileparts(fileparts(matlab.desktop.editor.getActiveFilename));
@@ -12,8 +20,7 @@ wavelength = 0.355;
 resolution = [1 1 1]*wavelength/10/NA;
 
 %% load RI profiles
-RI_metalens = load_RI_data('optimized_RI.h5');
-
+RI_metalens = load_RI_data('optimized_RI_no_pad.h5');
 phantom_params=PHANTOM.get_default_parameters();
 phantom_params.outer_size = size(RI_metalens);
 phantom_params.resolution = resolution;
@@ -21,9 +28,9 @@ phantom_params.wavelength = wavelength;
 phantom_params.cd0 = dirname;
 phantom_params.name=["PDMS","TiO2", "Microchem SU-8 2000"];
 phantom_params.thickness = [wavelength 0.15 size(RI_metalens)*resolution(3)];
-RI_flat= PHANTOM.get_TiO2_mask(phantom_params); % PDMS, TiO2, SU-8 layered structure 
+RI_flat= PHANTOM.get_TiO2_mask(phantom_params); % PDMS, TiO2, SU-8 layered structure
 
-RI_homogeneous = zeros('like',RI_metalens);
+RI_homogeneous = zeros(size(RI_metalens),'like',RI_metalens);
 RI_homogeneous(:) = real(get_RI(dirname,"PDMS",wavelength));
 
 RI_patterns = struct( ...
@@ -49,27 +56,6 @@ params.size=size(RI_metalens); % 3D volume grid
 params.return_3D = true;
 params.verbose = false;
 
-%1-1 CBS parameters
-params_CBS=params;
-params_CBS.use_GPU=true;
-params_CBS.boundary_thickness = [0 0 20];
-[minRI, maxRI] = bounds(RI_metalens,"all");
-params_CBS.RI_bg = double(sqrt((minRI^2+maxRI^2)/2));
-
-%1-2 FDTD parameters
-params_FDTD=params;
-params_FDTD.use_GPU=false;
-params_FDTD.boundary_thickness = [0 0 0];
-params_FDTD.RI_bg=real(get_RI(dirname,"PDMS", wavelength));
-params_FDTD.is_plane_wave = true;
-params_FDTD.PML_boundary = [false false true];
-
-forward_solver_list = { ...
-    FORWARD_SOLVER_CONVERGENT_BORN(params_CBS), ...
-    FORWARD_SOLVER_FDTD(params_FDTD) ...
-};
-solver_num = length(forward_solver_list);
-
 %% incident field parameters
 field_generator_params=params;
 field_generator_params.illumination_number=1;
@@ -79,12 +65,38 @@ input_field=field_generator.get_fields();
 
 %% solve the foward problem
 RI_type = 'metalens';
+RI = RI_patterns.(RI_type);
+
+%1-1 CBS parameters
+params_CBS=params;
+params_CBS.use_GPU=true;
+params_CBS.boundary_thickness = [0 0 5];
+[minRI, maxRI] = bounds(RI,"all");
+params_CBS.RI_bg = real(get_RI(dirname,"Microchem SU-8 2000",wavelength));
+params_CBS.max_attenuation_width = [0 0 5];
+
+%1-2 FDTD parameters
+params_FDTD=params;
+params_FDTD.use_GPU=false;
+params_FDTD.boundary_thickness = [0 0 0];
+params_FDTD.RI_bg=real(get_RI(dirname,"PDMS", wavelength));
+params_FDTD.is_plane_wave = true;
+params_FDTD.PML_boundary = [false false true];
+params_FDTD.fdtd_temp_dir = fullfile(dirname,'test/FDTD_TEMP');
+
+forward_solver_list = { ...
+    FORWARD_SOLVER_CONVERGENT_BORN(params_CBS), ...
+    FORWARD_SOLVER_FDTD(params_FDTD) ...
+};
+solver_num = length(forward_solver_list);
 
 E_field_rst = cell(solver_num,1);
 for isolver = 1:solver_num
     forward_solver = forward_solver_list{isolver};
-    forward_solver.set_RI(RI_patterns.(RI_type));
+    forward_solver.set_RI(RI);
+    tic;
     [~, ~, E_field_rst{isolver}] = forward_solver.solve(input_field);
+    toc;
 end
 
 %% draw results
@@ -93,18 +105,25 @@ concat_intensity = cat(2,intensity_list{:});
 figure('Name','Intensity: CBS / FDTD');
 orthosliceViewer(concat_intensity);
 colormap parula
+center_RI = round(size(intensity_list{1},1:2)/2);
+scale_xy = (1:size(intensity_list{1},1))*resolution(1);
+scale_z = (1:size(intensity_list{1},3))*resolution(3);
 
+max_val = max(concat_intensity, [], 'all')*1.1; 
 figure;
-subplot(2,2,[1,2]);
-imagesc(squeeze(concat_intensity(round(params.size(2)/2),:,:))');
-colormap parula;
-yline(41,'--yellow');
-subplot(2,2,3);
-plot(intensity_list{1}(:,41,41));
-title('CBS')
-subplot(2,2,4);
-plot(intensity_list{2}(:,41,41));
-title('FDTD')
+for i = 1:2
+    subplot(2,2,i);
+    imagesc(scale_xy, scale_xy, squeeze(intensity_list{i}(center_RI(1),:,:))',[0 max_val]);
+    xline(center_RI(1),'--yellow');
+    colormap parula;
+end
+subplot(2,2,[3 4]);
+hold on;
+plot(scale_z,squeeze(intensity_list{1}(center_RI(1),center_RI(2),:)));
+plot(scale_z,squeeze(intensity_list{2}(center_RI(1),center_RI(2),:)));
+legend('CBS','FDTD')
+ylim([0 max_val]);
+
 %%
 function [volume_RI,RI_params] = load_RI_data(filename)
     % save volume_RI and simulation condition in file_path (HDF5 format)
