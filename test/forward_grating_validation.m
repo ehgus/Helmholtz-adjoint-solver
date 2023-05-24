98f5f96de201864f3974ee3df69fe4beeaa504b5
Dohyeon Lee <dleh428@kaist.ac.kr>
2023-05-24 20:26:23 +0900

    update grating pattern validation code

--- test/forward_grating_validation.m
+++ test/forward_grating_validation.m
@@ -8,14 +8,27 @@ addpath(genpath(dirname));
 NA=1;
 oversampling_rate = 1;
 % load RI profiles
-[RI_grating, ~, wavelength] = load_RI(fullfile(fileparts(matlab.desktop.editor.getActiveFilename),'modulated_grating.mat'));
-resolution = [0.01 0.01 0.01];
-RI_bg = 1.4;
-RI_grating = padarray(reshape(RI_grating,1,[]),[numel(RI_grating)-1 0 15],'replicate','post');
-RI_grating = padarray(RI_grating,[0 0 10],RI_bg,'pre');
-RI_grating = padarray(RI_grating,[0 0 80],RI_bg,'post');
+[RI_grating_pattern, ~, wavelength] = load_RI(fullfile(fileparts(matlab.desktop.editor.getActiveFilename),'CBS_optimized grating.mat'));
+resolution = 0.01;
+mask_width = 0.15;
+grid_size = [100 100 105];
+
+database = RefractiveIndexDB();
+PDMS = database.material("organic","(C2H6OSi)n - polydimethylsiloxane","Gupta");
+TiO2 = database.material("main","TiO2","Siefke");
+Microchem_SU8_2000 = database.material("other","resists","Microchem SU-8 2000");
+RI_list = cellfun(@(func) real(func(wavelength)), {PDMS TiO2 Microchem_SU8_2000});
+RI_list(2) = 0;
+thickness_pixel = [0.20 mask_width]/resolution;
+RI_grating = phantom_plate(grid_size, RI_list, thickness_pixel);
+RI_grating(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) = RI_grating(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) + RI_grating_pattern(1,:,1);
+%oversampling
 resolution = resolution/oversampling_rate;
-RI_grating = imresize3(RI_grating, oversampling_rate, 'nearest');
+if oversampling_rate < 1
+    RI_grating = imresize3(RI_grating, oversampling_rate, 'linear');
+elseif oversampling_rate > 1
+    RI_grating = imresize3(RI_grating, oversampling_rate, 'nearest');
+end
 
 %% set optical parameters
 
@@ -27,7 +40,7 @@ MULTI_GPU=false; % Use Multiple GPU?
 %1-0 common optical parameters
 params.NA=NA; % Numerical aperture
 params.wavelength=wavelength; % [um]
-params.resolution=resolution; % 3D Voxel size [um]
+params.resolution=resolution*ones(1,3); % 3D Voxel size [um]
 params.use_abbe_sine=false; % Abbe sine condition according to demagnification condition
 params.vector_simulation=true; % True/false: dyadic/scalar Green's function
 params.size=size(RI_grating); % 3D volume grid
@@ -43,12 +56,12 @@ input_field=FieldGenerator.get_field(field_generator_params);
 %1-1 CBS parameters
 params_CBS=params;
 params_CBS.use_GPU=true;
-params_CBS.boundary_thickness = [0 0 15];
-params_CBS.field_attenuation = [0 0 15];
-params_CBS.field_attenuation_sharpness = 1;
+params_CBS.boundary_thickness = [0 0 5];
+params_CBS.field_attenuation = [0 0 5];
+params_CBS.field_attenuation_sharpness = 0.5;
 params_CBS.potential_attenuation = [0 0 4];
-params_CBS.potential_attenuation_sharpness = 1;
-params_CBS.RI_bg = RI_bg;
+params_CBS.potential_attenuation_sharpness = 0.5;
+params_CBS.RI_bg = RI_list(3);
 
 %1-2 FDTD parameters
 params_FDTD=params;
@@ -67,6 +80,7 @@ solver_num = length(forward_solver_list);
 
 E_field_rst = cell(solver_num,1);
 H_field_rst = cell(solver_num,1);
+
 for isolver = 1:solver_num
     forward_solver = forward_solver_list{isolver};
     save_title = sprintf("grating_pattern_%s_oversample_%d.mat",class(forward_solver), oversampling_rate);
@@ -86,38 +100,49 @@ for isolver = 1:solver_num
 end
 
 %% draw results
-intensity_list = arrayfun(@(x)(sum(abs(x{1}).^2,4)), E_field_rst,'UniformOutput',false);
-concat_intensity = cat(2,intensity_list{:});
-figure('Name','Intensity: CBS / FDTD');
-orthosliceViewer(concat_intensity);
-colormap parula
-center_RI = round(size(intensity_list{1},1:2)/2);
-scale_xy = (1:size(intensity_list{1},1))*resolution(1);
-scale_z = (1:size(intensity_list{1},3))*resolution(3);
+E_intensity_list = arrayfun(@(x)(sum(abs(x{1}).^2,4)), E_field_rst,'UniformOutput',false);
+H_intensity_list = arrayfun(@(x)(sum(abs(x{1}).^2,4)), H_field_rst,'UniformOutput',false);
+E_concat_intensity = cat(2,E_intensity_list{:});
+H_concat_intensity = cat(2,H_intensity_list{:});
 
-max_val = max(concat_intensity, [], 'all')*1.1; 
+% E intensity 
+figure('Name','|E|^2: CBS / FDTD');
+orthosliceViewer(E_concat_intensity);
+colormap parula
+
+% H intensity
+figure('Name','|H|^2: CBS / FDTD');
+orthosliceViewer(H_concat_intensity);
+colormap parula
+
+center_RI = round(size(E_intensity_list{1},1:2)/2);
+scale_xy = (1:size(E_intensity_list{1},1))*resolution;
+scale_z = (1:size(E_intensity_list{1},3))*resolution;
+
+max_val = max(E_concat_intensity, [], 'all')*1.1;
 figure;
 for i = 1:2
     subplot(2,2,i);
-    imagesc(scale_xy, scale_xy, squeeze(intensity_list{i}(center_RI(1),:,:))',[0 max_val]);
+    imagesc(scale_xy, scale_xy, squeeze(E_intensity_list{i}(center_RI(1),:,:))',[0 max_val]);
     xline(center_RI(1),'--yellow');
     colormap parula;
 end
 subplot(2,2,[3 4]);
 hold on;
-plot(scale_z,squeeze(intensity_list{1}(center_RI(1),center_RI(2),:)));
-plot(scale_z,squeeze(intensity_list{2}(center_RI(1),center_RI(2),:)));
+plot(scale_z,squeeze(E_intensity_list{1}(center_RI(1),center_RI(2),:)));
+plot(scale_z,squeeze(E_intensity_list{2}(center_RI(1),center_RI(2),:)));
 legend('CBS','FDTD')
 ylim([0 max_val]);
-%H field
-H_intensity_list = arrayfun(@(x)(sum(abs(x{1}(:,:,:,3)).^2,4)), H_field_rst,'UniformOutput',false);
-figure('Name','H field Intensity: CBS / FDTD');
-orthosliceViewer(cat(2,H_intensity_list{1}, H_intensity_list{2}));
-colormap parula;
-%% H field for each axis
-H_intensity_diff=abs(abs(H_field_rst{2})-abs(H_field_rst{1}));
-for axis = 1:3
-    disp(num2str(median(H_intensity_diff(:,:,:,axis),'all')))
+
+% MSE value
+% Set center of phase to be the same
+center_position = floor(size(E_field_rst{1},1:3)/2)+1;
+for i = 1:2
+    center_field = E_field_rst{i}(center_position(1), center_position(2), center_position(3),1);
+    center_field = center_field./abs(center_field);
+    E_field_rst{i} = E_field_rst{i}./center_field;
 end
-orthosliceViewer(cat(2,H_intensity_diff(:,:,:,1),H_intensity_diff(:,:,:,2),H_intensity_diff(:,:,:,3)));
-colormap parula;
+MSE_test = mean(abs(E_field_rst{1}-E_field_rst{2}).^2, 'all');
+fprintf("MSE test result(E): %f\n",MSE_test);
+MSE_test = mean(abs(H_field_rst{1}-H_field_rst{2}).^2, 'all');
+fprintf("MSE test result(H): %f\n",MSE_test);
