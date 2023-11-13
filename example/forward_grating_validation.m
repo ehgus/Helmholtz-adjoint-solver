9ea2b753e954679e4b41876e05a85750afc1dfba
Dohyeon Lee <dleh428@kaist.ac.kr>
2023-11-13 11:34:10 +0900

    modify grating validation code

--- example/forward_grating_validation.m
+++ example/forward_grating_validation.m
@@ -7,9 +7,13 @@ addpath(genpath(dirname));
 
 oversampling_rate = 1;
 % load RI profiles
-sim_type = 'CBS'; % CBS, 2D_FDTD
+sim_type_list = ["CBS","2D_FDTD"];
 NA=1;
-[RI_grating_pattern, ~, wavelength] = load_RI(fullfile(fileparts(matlab.desktop.editor.getActiveFilename),sprintf('%s_optimized grating.mat',sim_type)));
+RI_grating_pattern = cell(1,2);
+for idx = 1:2
+    sim_type = sim_type_list{idx};
+    [RI_grating_pattern{idx}, ~, wavelength] = load_RI(fullfile(fileparts(matlab.desktop.editor.getActiveFilename),sprintf('%s_optimized grating.mat',sim_type)));
+end
 resolution = 0.01;
 mask_width = 0.15;
 grid_size = [11 100 200];
@@ -21,19 +25,25 @@ Microchem_SU8_2000 = database.material("other","resists","Microchem SU-8 2000");
 RI_list = cellfun(@(func) real(func(wavelength)), {PDMS TiO2 Microchem_SU8_2000});
 RI_list(2) = 0;
 thickness_pixel = [0.2 mask_width]/resolution;
-RI_grating = phantom_plate(grid_size, RI_list, thickness_pixel);
-RI_grating(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) = RI_grating(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) + RI_grating_pattern(1,:,1);
-%oversampling
-resolution = resolution/oversampling_rate;
-if oversampling_rate < 1
-    RI_grating = imresize3(RI_grating, oversampling_rate, 'linear');
-elseif oversampling_rate > 1
-    RI_grating = imresize3(RI_grating, oversampling_rate, 'nearest');
-end
+background_plate = phantom_plate(grid_size, RI_list, thickness_pixel);
+RI_grating = cell(1,2);
 
 figure('Name','grating pattern')
-imagesc(squeeze(RI_grating_pattern(1,:,:))', real([PDMS(wavelength), TiO2(wavelength)]));
-colormap gray;
+for idx = 1:2
+    RI_grating{idx} = background_plate;
+    RI_grating{idx}(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) = RI_grating{idx}(:,:,thickness_pixel(1)+1:sum(thickness_pixel(1:2))) + RI_grating_pattern{idx}(1,:,1);
+    %oversampling
+    resolution = resolution/oversampling_rate;
+    if oversampling_rate < 1
+        RI_grating{idx} = imresize3(RI_grating{idx}, oversampling_rate, 'linear');
+    elseif oversampling_rate > 1
+        RI_grating{idx} = imresize3(RI_grating{idx}, oversampling_rate, 'nearest');
+    end
+
+    subplot(2,1,idx)
+    imagesc(squeeze(RI_grating_pattern{idx}(1,:,:))', real([PDMS(wavelength), TiO2(wavelength)]));
+    colormap gray;
+end
 %% set optical parameters
 
 %0 gpu accelerator
@@ -46,7 +56,7 @@ params.NA=NA; % Numerical aperture
 params.wavelength=wavelength; % [um]
 params.resolution=resolution*ones(1,3); % 3D Voxel size [um]
 params.use_abbe_sine=false; % Abbe sine condition according to demagnification condition
-params.size=size(RI_grating); % 3D volume grid
+params.size=size(RI_grating{1}); % 3D volume grid
 params.verbose = false;
 params.RI_bg = RI_list(1);
 
@@ -76,29 +86,24 @@ params_FDTD.is_plane_wave = true;
 params_FDTD.PML_boundary = [false false true];
 params_FDTD.fdtd_temp_dir = fullfile(dirname,'test/FDTD_TEMP');
 params_FDTD.hide_GUI = true;
-forward_solver_list = { ...
-    ConvergentBornSolver(params_CBS), ...
-    FDTDsolver(params_FDTD) ...
-};
-solver_num = length(forward_solver_list);
+forward_solver=ConvergentBornSolver(params_CBS);
+sim_num = length(sim_type_list);
+E_field_rst = cell(1,sim_num);
+H_field_rst = cell(1,sim_num);
 
-E_field_rst = cell(solver_num,1);
-H_field_rst = cell(solver_num,1);
-
-for isolver = 1:solver_num
-    forward_solver = forward_solver_list{isolver};
-    save_title = sprintf("grating_pattern_%s_oversample_%d_%s.mat",class(forward_solver), oversampling_rate, sim_type);
+for idx = 1:sim_num
+    save_title = sprintf("grating_pattern_%s_oversample_%d_%s.mat",sim_type_list(idx), oversampling_rate, sim_type);
     if isfile(save_title)
         load(save_title)
-        E_field_rst{isolver} = E_field_3D;
-        H_field_rst{isolver} = H_field_3D;
+        E_field_rst{idx} = E_field_3D;
+        H_field_rst{idx} = H_field_3D;
         continue
     end
-    forward_solver.set_RI(RI_grating);
+    forward_solver.set_RI(RI_grating{idx});
     tic;
-    [E_field_rst{isolver}, H_field_rst{isolver}] = forward_solver.solve(current_source);
-    E_field_3D = E_field_rst{isolver};
-    H_field_3D = H_field_rst{isolver};
+    [E_field_rst{idx}, H_field_rst{idx}] = forward_solver.solve(current_source);
+    E_field_3D = E_field_rst{idx};
+    H_field_3D = H_field_rst{idx};
     toc;
     save(save_title, 'E_field_3D','H_field_3D');
 end
@@ -136,7 +141,7 @@ subplot(2,2,[3 4]);
 hold on;
 plot(scale_z,squeeze(E_intensity_list{1}(center_RI(1),center_RI(2),:)));
 plot(scale_z,squeeze(E_intensity_list{2}(center_RI(1),center_RI(2),:)));
-legend('CBS','FDTD')
+legend(sim_type_list(1),sim_type_list(2))
 ylim([0 max_E_val]);
 % field transmittance for each plane wave mode
 target_angle_mode = -4:4;
@@ -146,11 +151,12 @@ eigen_H_field = cell(1,length(target_transmission));
 relative_transmission_ref = zeros(1,length(target_transmission));
 
 params_CBS.RI_bg = RI_list(3);
-CBS_forward_solver = ConvergentBornSolver(params_CBS);
-impedance = 377/CBS_forward_solver.RI_bg;
-Nsize = CBS_forward_solver.size + 2*CBS_forward_solver.boundary_thickness_pixel;
+forward_solver = ConvergentBornSolver(params_CBS);
+impedance = 377/forward_solver.RI_bg;
+Nsize = forward_solver.size + 2*forward_solver.boundary_thickness_pixel;
 Nsize(4) = 3;
 
+% 
 for idx = 1:length(target_angle_mode)
     illum_order = target_angle_mode(idx);
     k_y = 2*pi*illum_order/(params_CBS.size(2)*params_CBS.resolution(2));
@@ -169,6 +175,8 @@ for idx = 1:length(target_angle_mode)
 end
 
 relative_transmission = zeros(1,length(target_transmission));
+
+% numerical view
 for idx = 1:length(E_field_rst)
     E_field = E_field_rst{idx};
     H_field = H_field_rst{idx};
@@ -177,8 +185,9 @@ for idx = 1:length(E_field_rst)
         eigen_S = poynting_vector(E_field, eigen_H_field{field_idx}) + poynting_vector(conj(eigen_E_field{field_idx}), conj(H_field));
         relative_transmission(field_idx) = sum(eigen_S(:,:,end,3),'all');
     end
-    fprintf("%s :",class(forward_solver_list{idx}));
+    fprintf("%10s :",sim_type_list(idx));
     disp(abs(relative_transmission./relative_transmission_ref).^2);
+    fprintf("       FoM = %f\n\n",sum((abs(relative_transmission./relative_transmission_ref).^2 -target_transmission).^2));
 end
 
 % theoretical view
@@ -198,8 +207,3 @@ for idx = 1:length(target_transmission)
 end
 fprintf("Ideal case :");
 disp(abs(relative_transmission./relative_transmission_ref).^2);
-
-
-
-
-
